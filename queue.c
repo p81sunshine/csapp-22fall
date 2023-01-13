feba9ffeeef1e7c24a8d0683da2525f74ed4e57e
p81sunshine <2985851315@qq.com>
2023-01-13 22:25:18 +0800

    This is the original buggy version of this lab

---
+++ queue.c
@@ -0,0 +1,205 @@
+/**
+ * @file queue.c
+ * @brief Implementation of a queue that supports FIFO and LIFO operations.
+ *
+ * This queue implementation uses a singly-linked list to represent the
+ * queue elements. Each queue element stores a string value.
+ *
+ * Assignment for basic C skills diagnostic.
+ * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
+ * Extended to store strings, 2018
+ *
+ * TODO: fill in your name and Andrew ID
+ * @author XXX <XXX@andrew.cmu.edu>
+ */
+
+#include "queue.h"
+#include "harness.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#define zero 0
+
+/**
+ * @brief Allocates a new queue
+ * @return The new queue, or NULL if memory allocation failed
+ */
+queue_t *queue_new(void) {
+    queue_t *q = malloc(sizeof(queue_t));
+    /* What if malloc returned NULL? */
+    if (q == NULL)
+        return NULL;
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = zero;
+    return q;
+}
+
+/**
+ * @brief Frees all memory used by a queue
+ * @param[in] q The queue to free
+ */
+void queue_free(queue_t *q) {
+    /* How about freeing the list elements and the strings? */
+    /* Free queue structure */
+    while (q->head->next != NULL) {
+        list_ele_t *tmp = q->head;
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+    }
+    free(q->head);
+    free(q);
+}
+
+/**
+ * @brief Attempts to insert an element at head of a queue
+ *
+ * This function explicitly allocates space to create a copy of `s`.
+ * The inserted element points to a copy of `s`, instead of `s` itself.
+ *
+ * @param[in] q The queue to insert into
+ * @param[in] s String to be copied and inserted into the queue
+ *
+ * @return true if insertion was successful
+ * @return false if q is NULL, or memory allocation failed
+ */
+bool queue_insert_head(queue_t *q, const char *s) {
+    list_ele_t *newh;
+    /* What should you do if the q is NULL? */
+    newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
+    /* Don't forget to allocate space for the string and copy it */
+    char *new_s = malloc(sizeof(s) * sizeof(char));
+    if (new_s == NULL)
+        return false;
+    strcpy(new_s, s);
+
+    newh->value = new_s;
+
+    /* What if either call to malloc returns NULL? */
+    newh->next = q->head;
+    q->head = newh;
+
+    /* Handle cases when insert the first element in the queue*/
+    q->size++;
+    if (q->size == 0) {
+        q->tail = q->head;
+    }
+
+    return true;
+}
+
+/**
+ * @brief Attempts to insert an element at tail of a queue
+ *
+ * This function explicitly allocates space to create a copy of `s`.
+ * The inserted element points to a copy of `s`, instead of `s` itself.
+ *
+ * @param[in] q The queue to insert into
+ * @param[in] s String to be copied and inserted into the queue
+ *
+ * @return true if insertion was successful
+ * @return false if q is NULL, or memory allocation failed
+ */
+bool queue_insert_tail(queue_t *q, const char *s) {
+    /* You need to write the complete code for this function */
+    /* Remember: It should operate in O(1) time */
+
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
+
+    char *new_s = malloc(sizeof(s) * sizeof(char));
+    if (new_s == NULL)
+        return false;
+
+    newh->value = new_s;
+
+    newh->next = q->tail;
+    q->tail = newh;
+
+    /* Handle cases when insert the first element in the queue*/
+    q->size++;
+
+    return true;
+}
+
+/**
+ * @brief Attempts to remove an element from head of a queue
+ *
+ * If removal succeeds, this function frees all memory used by the
+ * removed list element and its string value before returning.
+ *
+ * If removal succeeds and `buf` is non-NULL, this function copies up to
+ * `bufsize - 1` characters from the removed string into `buf`, and writes
+ * a null terminator '\0' after the copied string.
+ *
+ * @param[in]  q       The queue to remove from
+ * @param[out] buf     Output buffer to write a string value into
+ * @param[in]  bufsize Size of the buffer `buf` points to
+ *
+ * @return true if removal succeeded
+ * @return false if q is NULL or empty
+ */
+bool queue_remove_head(queue_t *q, char *buf, size_t bufsize) {
+    /* You need to fix up this code. */
+    if (q == NULL || q->size == zero)
+        return false;
+    list_ele_t *tmp;
+    tmp = q->head;
+    q->head = q->head->next;
+
+    if (buf != NULL) {
+        for (unsigned long i = 0; i < bufsize - 1; i++) {
+            buf[i] = tmp->value[i];
+        }
+        buf[bufsize - 1] = '\0';
+    }
+    free(tmp->value);
+    free(tmp);
+    q->size--;
+    return true;
+}
+
+/**
+ * @brief Returns the number of elements in a queue
+ *
+ * This function runs in O(1) time.
+ *
+ * @param[in] q The queue to examine
+ *
+ * @return the number of elements in the queue, or
+ *         0 if q is NULL or empty
+ */
+size_t queue_size(queue_t *q) {
+    /* You need to write the code for this function */
+    /* Remember: It should operate in O(1) time */
+    return (size_t)q->size;
+}
+
+/**
+ * @brief Reverse the elements in a queue
+ *
+ * This function does not allocate or free any list elements, i.e. it does
+ * not call malloc or free, including inside helper functions. Instead, it
+ * rearranges the existing elements of the queue.
+ *
+ * @param[in] q The queue to reverse
+ */
+void queue_reverse(queue_t *q) {
+    /* You need to write the code for this function */
+    list_ele_t *cur = q->head;
+    list_ele_t *next = NULL;
+    list_ele_t *pre = NULL;
+
+    while (cur != NULL) {
+        next = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = next;
+    }
+}
